e140c9cf680e3aafe3facb75c502f3062ccaf1a5
ispeterthere <45906868+ispeterthere@users.noreply.github.com>
2021-02-15 16:46:06 -0500

    Update Files needed

--- path_finder.py
+++
@@ -1,9 +0,0 @@
-from _node import Node
-
-
-# Calculate the L distance between Nodes
-class path_finder():
-    def heuristic(point1, point2):
-        x1, y1 = point1
-        x2, y2 = point2
-        return abs(x1 - x2) + abs(y1 - y2)
